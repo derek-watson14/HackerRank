f96b0659e51ff2b62ebc003f44481c71f42e8ea4
Derek Watson <derek.watson92@gmail.com>
2020-12-07 22:40:51 -0800

    completed minimum absolute difference challenge

---
+++ easy/greedy-algorithms/minAbsoluteDifference.js
@@ -0,0 +1,16 @@
+// https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+
+function minimumAbsoluteDifference(arr) {
+  let minAD = Infinity;
+  arr.sort().forEach((num, ind, arr) => {
+    if (ind < arr.length - 1) {
+      const pairDiff = Math.abs(num - arr[ind + 1]);
+      minAD = pairDiff < minAD ? pairDiff : minAD;
+    }
+  });
+  return minAD;
+}
+
+const test = [-59, -36, -13, 1, -53, -92, -2, -96, -54, 75];
+
+console.log(minimumAbsoluteDifference(test));
